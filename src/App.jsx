83d818a45c4d3704672f2764250a1155cfbfcb25
pedromalta14 <rm95182@estudante.fieb.edu.br>
2025-03-14 09:38:51 -0300

    projeto2f

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Component } from "react" ;
+
+class App extends Component{
+
+ render(){
+
+   return (
+      <div>
+          <h3>Aula React Vite</h3>
+          <p>Retomada de Conteúdo</p>
+      </div>
+
+   )
+ }
+}
+export default App
+
